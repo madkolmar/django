b133819e803122f763d86d4d07641dc672441dfd
XoJIog <frolov@informatics.ru>
2013-07-10 12:54:51 +0400

    Tests added

---
+++ tests/custom_fields_managers/models.py
@@ -0,0 +1,41 @@
+"""
+Custom manager for relationship fields.
+
+Using the ``manager`` and ``reverse_manager`` when creating a ForeignKey, you can
+choose custom managers used for direct and reverse relationship.
+
+Note: You will rarely need to define a custom ``manager`` for a ForeignKey,
+      execept when you want to filter out the queryset that will be
+      used to retrieve the related object.
+
+"""
+
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+class ArticlesManager(models.Manager):
+    def get_articles_starting_with_a(self):
+        return self.filter(name__startswith='a')
+
+class DeletedManager(models.Manager):
+    def get_queryset(self):
+        return super(DeletedManager, self).get_queryset().filter(deleted=False)
+
+
+@python_2_unicode_compatible
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    deleted = models.BooleanField(default=False)
+
+
+@python_2_unicode_compatible
+class Article(models.Model):
+    name = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, manager=DeletedManager, reverse_manager=ArticlesManager, related_name='articles')
+
+    def __str__(self):
+        return self.name
+
